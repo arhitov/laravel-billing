0a338e3148426a9f22999fa7e1323d0cc18e0e6b
clgsru <clgsru@gmail.com>
2024-04-11 13:32:28 +0300

    Rounding mod no used

--- config/billing.php
+++ config/billing.php
@@ -86,16 +86,6 @@ return [
     'rounding' => [
 
         /**
-         * Rounding type
-         * NULL - No rounding. The raw value is written to the database
-         * PHP_ROUND_HALF_UP - Rounds num away from zero when it is half way there, making 1.5 into 2 and -1.5 into -2.
-         * PHP_ROUND_HALF_DOWN - Rounds num towards zero when it is half way there, making 1.5 into 1 and -1.5 into -1.
-         * PHP_ROUND_HALF_EVEN - Rounds num towards the nearest even value when it is half way there, making both 1.5 and 2.5 into 2.
-         * PHP_ROUND_HALF_ODD - Rounds num towards the nearest odd value when it is half way there, making 1.5 into 1 and 2.5 into 3.
-         */
-        'mod' => null,
-
-        /**
          * Precision of float value.
          */
         'precision' => 4
